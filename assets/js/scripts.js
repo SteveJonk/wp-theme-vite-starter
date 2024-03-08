1ab99272b0eb4848d36bf25b4bbdc3ee9816be2b
Steve Jonk <stevejonk1993@gmail.com>
2024-03-08 11:47:27 +0100

    feat: setup base

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+import { imported } from './imported';
+
+imported();
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Handler when the DOM is fully loaded
+  console.log('js executed...');
+});
---
+++ dist/assets/main-ecced5ab.js
@@ -0,0 +1,1 @@
+const e=()=>{alert("imorted js file is working!")};e();document.addEventListener("DOMContentLoaded",function(){console.log("js executed...")});
